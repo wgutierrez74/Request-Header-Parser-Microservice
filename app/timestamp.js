ba59f5dcce29db1af07d55eb8d2b475e87a2bb55
System <none>
2017-07-25 20:16:58 +0000

    Checkpoint

--- app/timestamp.js
+++ app/timestamp.js
@@ -1,3 +1,36 @@
 
 
-module.exports
+module.exports = function(app){
+  
+  app.route('/', function(req, res){
+    
+    res.sendFile(process.cwd()+ 'views/index.html');
+    
+  });
+  
+  
+  app.route('/:query', function(req, res){
+    
+    var query = req.params.query;
+    
+    if(Number(query)){
+      unixToNatural()
+    }
+    else{
+      
+    }
+    
+    
+    
+    
+    res.send();
+    
+  });
+  
+  
+  
+  
+  
+  
+  
+};
